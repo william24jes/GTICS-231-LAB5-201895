0cb9fe6f1dd5277c0801d8590d282c15e864c8d6
william <william24jes@hotmail.com>
2023-04-25 21:15:44 -0500

    a

---
+++ src/main/java/com/example/gtics231lab5201895/Gtics231Lab5201895Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics231lab5201895;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gtics231Lab5201895Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gtics231Lab5201895Application.class, args);
+    }
+
+}
